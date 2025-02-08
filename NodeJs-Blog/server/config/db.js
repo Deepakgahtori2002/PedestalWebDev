0efb111921cbd9ae055955f8b70fa806162fd29e
Deepakgahtori2002 <deep2001gahtori@gmail.com>
2025-02-08 13:19:34 +0530

    files added

---
+++ NodeJs-Blog/server/config/db.js
@@ -0,0 +1,30 @@
+// const mongoose = require(mongoose);
+// const connectDB = async () => {
+
+//     try {
+//         mongoose.set(strictQuery, false);
+//         const conn = await mongoose.connect(process.env.MONGODB_URI);
+//         console.log(`Database Connected: ${conn.connection.host}`);
+//     } catch (error){
+//         console.log(error);
+    
+
+//     }
+// }
+
+// export default connectDB;
+
+const mongoose = require('mongoose');
+const connectDB = async () => {
+  
+  try {
+    mongoose.set('strictQuery', false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+
+}
+
+module.exports = connectDB;
